e2d3a33757f4f5b40079c2032ac2b8d6bf7cea42
Taos Transue <spacechips@rocketmail.com>
2023-01-01 00:40:36 -0800

    start to generating denormalized django models

--- server/models.py
+++ server/models.py
@@ -1,3 +1,73 @@
+import json
+from pathlib import Path
+import enum
 from django.db import models
+import server.ob_item_types as obit
 
-# Create your models here.
+
+NAME_LEN = 100
+STR_LEN = 500
+URL_LEN = 1000
+TIME_STR_LEN = 100
+OB_TAXONOMY = obit.load_ob_taxonomy()
+
+
+class OpenAPIType(enum.Enum):
+    BOOLEAN = 'boolean'
+    INTEGER = 'integer'
+    NUMBER = 'number'
+    STRING = 'string'
+
+
+def field(openapi_type, ob_item_type, **kwargs):
+    return dict(
+        openapi_type=openapi_type,
+        ob_item_type=obit.json_to_item_type(ob_item_type, OB_TAXONOMY),
+        **kwargs
+    )
+
+
+def Decimals_field():
+    return models.IntegerField()
+
+
+def EndTime_field():
+    return models.CharField(max_length=TIME_STR_LEN)
+
+
+def Precision_field():
+    return models.IntegerField()
+
+
+def StartTime_field():
+    return models.CharField(max_length=TIME_STR_LEN)
+
+
+def Unit_field(max_length=STR_LEN):
+    return models.CharField(max_length=max_length)
+
+
+def Value_field(max_length=STR_LEN):
+    return models.CharField(max_length=max_length)
+
+
+def add_model_fields(m):
+    for fname, v in m.field_metadata.items():
+        openapi_type, ob_item_type = v['openapi_type'], v['ob_item_type']
+        for p, f in zip(['Decimals', 'EndTime', 'Precision', 'StartTime', 'Unit', 'Value'], [Decimals_field, EndTime_field, Precision_field, StartTime_field, Unit_field, Value_field]):
+            setattr(m, f'{fname}_{p}', f())
+
+
+class Product(models.Model):
+    field_metadata = {
+        'Description': field(OpenAPIType.STRING, 'StringItemType', max_length=STR_LEN),
+        'FileFolderURL': field(OpenAPIType.STRING, 'StringItemType', max_length=URL_LEN),
+    }
+
+    class Meta:
+        abstract = True
+
+
+all_models = [Product]
+for m in all_models:
+    add_model_fields(m)
---
+++ server/ob_item_types.py
@@ -0,0 +1,57 @@
+import json
+from pathlib import Path
+from typing import Tuple
+from dataclasses import dataclass
+
+
+OB_TAXONOMY_FILEPATH = Path(__file__).parent / 'references' / 'Master-OB-OpenAPI.json'
+
+
+@dataclass(frozen=True)
+class ItemType:
+    name: str
+    description: str
+
+
+@dataclass(frozen=True)
+class ItemTypeValue:
+    id: str
+    label: str
+    description: str
+
+
+@dataclass(frozen=True)
+class Enum(ItemTypeValue):
+    pass
+
+
+@dataclass(frozen=True)
+class Unit(ItemTypeValue):
+    pass
+
+
+@dataclass(frozen=True)
+class ItemTypeEnum(ItemType):
+    enums: Tuple[ItemTypeValue]
+
+
+@dataclass(frozen=True)
+class ItemTypeUnit(ItemType):
+    units: Tuple[ItemTypeValue]
+
+
+def load_ob_taxonomy():
+    with open(OB_TAXONOMY_FILEPATH) as f:
+        return json.load(f)
+
+
+def json_to_item_type(name: str, ob_taxonomy: dict):
+    it = ob_taxonomy['x-ob-item-types'][name]
+    get_item_type_values = lambda key: tuple(val_class(id, v['label'], v['description'])
+                                             for id, v in it[key].items())
+    if 'enums' in it:
+        return ItemTypeEnum(name, it['description'], get_item_type_values('enums'))
+    elif 'units' in it:
+        return ItemTypeUnit(name, it['description'], get_item_type_values('units'))
+    else:
+        return ItemType(name, it['description'])
