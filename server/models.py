5f2910c385c64041ee7555d853ed67e39ddb4750
Taos Transue <spacechips@rocketmail.com>
2023-01-01 20:42:27 -0800

    refactored django model generation

--- server/models.py
+++ server/models.py
@@ -1,5 +1,4 @@
-import json
-from pathlib import Path
+from collections import namedtuple
 import enum
 from django.db import models
 import server.ob_item_types as obit
@@ -14,104 +13,12 @@ DECIMAL_MAX_DIGITS = DECIMAL_PLACES * 3
 OB_TAXONOMY = obit.load_ob_taxonomy()
 
 
-class OpenAPIType(enum.Enum):
-    BOOLEAN = 'boolean'
-    INTEGER = 'integer'
-    NUMBER = 'number'
-    STRING = 'string'
-
-
-def field(openapi_type, ob_item_type, **kwargs):
-    return dict(
-        openapi_type=openapi_type,
-        ob_item_type=obit.json_to_item_type(ob_item_type, OB_TAXONOMY),
-        **kwargs
-    )
-
-
-def Decimals(name):
-    return 'Decimals', models.IntegerField(f'{name} Decimals')
-
-
-def EndTime(name):
-    return 'EndTime', models.CharField(f'{name} EndTime', max_length=TIME_STR_LEN)
-
-
-def Precision(name):
-    return 'Precision', models.IntegerField(f'{name} Precision')
-
-
-def StartTime(name):
-    return 'StartTime', models.CharField(f'{name} StartTime', max_length=TIME_STR_LEN)
-
-
-def Unit(name, ob_item_type: obit.ItemTypeUnit):
-    max_length = max(len(u.id) for u in ob_item_type.units)
-    return 'Unit', models.CharField(f'{name} Unit', max_length=max_length)
-
-
-def Value(name, field_class, ob_item_type=None, **kwargs):
-    if field_class is models.CharField:
-        if ob_item_type.__class__ is obit.ItemTypeEnum:
-            kwargs['max_length'] = max(len(e.id) for e in ob_item_type.enums)
-        else:
-            kwargs['max_length'] = STR_LEN
-    return 'Value', field_class(f'{name} Value', **kwargs)
-
-
-def TaxonomyElement(name):
-    return [EndTime(name), StartTime(name)]
-
-
-def TaxonomyElementBoolean(name):
-    fields = TaxonomyElement(name)
-    fields.append(Value(name, models.BooleanField))
-    return fields
-
-
-def TaxonomyElementInteger(name, ob_item_type: obit.ItemType):
-    fields = [Value(name, models.IntegerField)]
-    if len(ob_item_type.units) > 0:
-        fields.append(Unit(name, ob_item_type))
-    return fields
-
-
-def TaxonomyElementNumber(name, ob_item_type: obit.ItemTypeUnit):
-    fields = TaxonomyElement(name)
-    fields.append(Value(name, models.DecimalField, max_digits=DECIMAL_MAX_DIGITS, decimal_places=DECIMAL_PLACES))
-    if len(ob_item_type.units) > 0:
-        fields.append(Unit(name, ob_item_type))
-    return fields
-
-
-def TaxonomyElementString(name, ob_item_type):
-    fields = TaxonomyElement(name)
-    fields.append(Value(name, models.CharField, ob_item_type=ob_item_type))
-    return fields
-
-
-def add_model_fields(attrs):
-    ob_elements = attrs['ob_elements']
-    for fname, v in ob_elements.items():
-        openapi_type, ob_item_type = v['openapi_type'], v['ob_item_type']
-        match openapi_type:
-            case OpenAPIType.BOOLEAN:
-                fields = TaxonomyElementBoolean(fname, ob_item_type)
-            case OpenAPIType.INTEGER:
-                fields = TaxonomyElementInteger(fname, ob_item_type)
-            case OpenAPIType.NUMBER:
-                fields = TaxonomyElementNumber(fname, ob_item_type)
-            case OpenAPIType.STRING:
-                fields = TaxonomyElementString(fname, ob_item_type)
-        for f in fields:
-            primitive, field = f
-            attrs[f'{fname}_{primitive}'] = field
-
-
 class ModelBase(models.base.ModelBase):
     def __new__(cls, name, bases, attrs, **kwargs):
         if 'ob_elements' in attrs:
-            add_model_fields(attrs)
+            for element in attrs['ob_elements']:
+                for primitive, field in element.django_model_fields().items():
+                    attrs[f'{element.name}_{primitive}'] = field
         return super().__new__(cls, name, bases, attrs, **kwargs)
 
 
@@ -123,24 +30,24 @@ class Model(models.Model, metaclass=ModelBase):
 
 
 class Dimensions(Model):
-    ob_elements = dict(
-        Height=field(OpenAPIType.NUMBER, 'LengthItemType'),
-        Length=field(OpenAPIType.NUMBER, 'LengthItemType'),
-        Mass=field(OpenAPIType.NUMBER, 'MassItemType'),
-        Weight=field(OpenAPIType.NUMBER, 'MassItemType'),
-        Width=field(OpenAPIType.NUMBER, 'LengthItemType'),
+    ob_elements = (
+        obit.OBElement('Height'),
+        obit.OBElement('Length'),
+        obit.OBElement('Mass'),
+        obit.OBElement('Weight'),
+        obit.OBElement('Width')
     )
 
 
 class Product(Model):
-    ob_elements = dict(
-        Description=field(OpenAPIType.STRING, 'StringItemType'),
-        FileFolderURL=field(OpenAPIType.STRING, 'StringItemType'),
-        ProdCode=field(OpenAPIType.STRING, 'StringItemType'),
-        ProdDatasheet=field(OpenAPIType.STRING, 'StringItemType'),
-        ProdMfr=field(OpenAPIType.STRING, 'StringItemType'),
-        ProdName=field(OpenAPIType.STRING, 'StringItemType'),
-        ProdType=field(OpenAPIType.STRING, 'ProdTypeItemType'),
+    ob_elements = (
+        obit.OBElement('Description'),
+        obit.OBElement('FileFolderURL'),
+        obit.OBElement('ProdCode', max_length=16),
+        obit.OBElement('ProdDatasheet'),
+        obit.OBElement('ProdMfr'),
+        obit.OBElement('ProdName'),
+        obit.OBElement('ProdType'),
     )
 
     class Meta:
--- server/ob_item_types.py
+++ server/ob_item_types.py
@@ -1,17 +1,39 @@
+import enum
 import json
 from pathlib import Path
 from typing import Tuple
 from dataclasses import dataclass
+from django.db import models
 
 
+NAME_LEN = 100
+STR_LEN = 500
+URL_LEN = 1000
+TIME_STR_LEN = 100
+DECIMAL_PLACES = 8
+DECIMAL_MAX_DIGITS = DECIMAL_PLACES * 3
 OB_TAXONOMY_FILEPATH = Path(__file__).parent / 'references' / 'Master-OB-OpenAPI.json'
+
+
+def load_ob_taxonomy():
+    with open(OB_TAXONOMY_FILEPATH) as f:
+        return json.load(f)
+
+
+OB_TAXONOMY = load_ob_taxonomy()
+
+
+class TaxonomyElement(enum.Enum):
+    Boolean = 'TaxonomyElementBoolean'
+    Integer = 'TaxonomyElementInteger'
+    Number = 'TaxonomyElementNumber'
+    String = 'TaxonomyElementString'
 
 
 @dataclass(frozen=True)
 class ItemType:
     name: str
     description: str
-
 
 @dataclass(frozen=True)
 class ItemTypeValue:
@@ -32,17 +54,163 @@ class Unit(ItemTypeValue):
 
 @dataclass(frozen=True)
 class ItemTypeEnum(ItemType):
-    enums: Tuple[ItemTypeValue]
+    values: Tuple[ItemTypeValue]
 
 
 @dataclass(frozen=True)
 class ItemTypeUnit(ItemType):
-    units: Tuple[ItemTypeValue]
+    values: Tuple[ItemTypeValue]
 
 
-def load_ob_taxonomy():
-    with open(OB_TAXONOMY_FILEPATH) as f:
-        return json.load(f)
+@dataclass(frozen=True)
+class ItemTypeGroup:
+    type: str
+    description: str
+    group: Tuple[str]
+
+
+@dataclass
+class OBElement:
+    name: str
+    description: str
+    superclass: str
+    item_type: ItemType
+    item_type_group: ItemTypeGroup
+
+    def __init__(self, name, **Value_opts):
+        allOf = OB_TAXONOMY['components']['schemas'][name]
+        superclass_ref, details = allOf['allOf']
+
+        self.name = name
+        self.description = details['description']
+        self.superclass = TaxonomyElement(superclass_ref['$ref'].split('/')[-1])
+        self.item_type = json_to_item_type(details['x-ob-item-type'], OB_TAXONOMY)
+        self.item_type_group = json_to_item_type_group(details['x-ob-item-type-group'], OB_TAXONOMY)
+        self.Value_opts = Value_opts
+
+    @property
+    def grouped_item_type(self):
+        if self.item_type_group is None:
+            return self.item_type
+        kwargs = dict(
+            name=self.item_type.name,
+            description=self.item_type.description
+        )
+        if self.item_type_has_enums or self.item_type_has_units:
+            kwargs['values'] = tuple(v for v in self.item_type.values if v.id in self.item_type_group.group)
+        return self.item_type.__class__(**kwargs)
+
+    @property
+    def item_type_has_enums(self):
+        return self._item_type_has_values(ItemTypeEnum)
+
+    @property
+    def item_type_has_units(self):
+        return self._item_type_has_values(ItemTypeUnit)
+
+    def django_model_fields(self):
+        match self.superclass:
+            case TaxonomyElement.Boolean:
+                return self._boolean_fields()
+            case TaxonomyElement.Integer:
+                return self._integer_fields()
+            case TaxonomyElement.Number:
+                return self._number_fields()
+            case TaxonomyElement.String:
+                return self._string_fields()
+
+    def _item_type_has_values(self, item_type_class):
+        nonempty_item_type_group = self.item_type_group is None or len(self.item_type_group.group) > 0
+        return (
+            self.item_type.__class__ is item_type_class
+            and len(self.item_type.values) > 0
+            and nonempty_item_type_group
+        )
+
+    def _field_name(self, primitive_name):
+        return f'{self.name}_{primitive_name}'
+
+    def _verbose_field_name(self, primitive_name):
+        return f'{self.name} {primitive_name}'
+
+    def _boolean_fields(self):
+        return dict(
+            EndTime=self._EndTime_field(),
+            StartTime=self._StartTime_field(),
+            Value=self._Value_field()
+        )
+
+    def _integer_fields(self):
+        fields = dict(
+            EndTime=self._EndTime_field(),
+            StartTime=self._StartTime_field(),
+            Value=self._Value_field()
+        )
+        if self.item_type_has_units:
+            fields['Unit'] = self._Unit_field()
+        return fields
+
+    def _number_fields(self):
+        fields = dict(
+            Decimals=self._Decimals_field(),
+            EndTime=self._EndTime_field(),
+            Precision=self._Precision_field(),
+            StartTime=self._StartTime_field(),
+            Value=self._Value_field()
+        )
+        if self.item_type_has_units:
+            fields['Unit'] = self._Unit_field()
+        return fields
+
+    def _string_fields(self):
+        return dict(
+            EndTime=self._EndTime_field(),
+            StartTime=self._StartTime_field(),
+            Value=self._Value_field()
+        )
+
+    def _Decimals_field(self):
+        return models.DecimalField(self._verbose_field_name('Decimals'),
+                                   max_digits=DECIMAL_MAX_DIGITS,
+                                   decimal_places=DECIMAL_PLACES,
+                                   blank=True, null=True)
+
+    def _EndTime_field(self):
+        return models.DateTimeField(self._verbose_field_name('EndTime'),
+                                    blank=True, null=True)
+
+    def _Precision_field(self):
+        return models.IntegerField(blank=True, null=True)
+
+    def _StartTime_field(self):
+        return models.DateTimeField(self._verbose_field_name('EndTime'),
+                                    blank=True, null=True)
+
+    def _Unit_field(self):
+        max_length = max(len(v.id) for v in self.grouped_item_type.values)
+        return models.CharField(self._verbose_field_name('Unit'),
+                                max_length=max_length, blank=True)
+
+    def _Value_field(self):
+        match self.superclass:
+            case TaxonomyElement.Boolean:
+                return models.BooleanField(self._verbose_field_name('Value'),
+                                           blank=True, null=True)
+            case TaxonomyElement.Integer:
+                return models.IntegerField(self._verbose_field_name('Value'),
+                                           blank=True, null=True)
+            case TaxonomyElement.Number:
+                return models.DecimalField(self._verbose_field_name('Value'),
+                                           max_digits=DECIMAL_MAX_DIGITS,
+                                           decimal_places=DECIMAL_PLACES,
+                                           blank=True, null=True)
+            case TaxonomyElement.String:
+                if self.item_type_has_enums:
+                    max_length = max(len(v.id) for v in self.grouped_item_type.values)
+                else:
+                    max_length = self.Value_opts.get('max_length', STR_LEN)
+                return models.CharField(self._verbose_field_name('Value'),
+                                        max_length=max_length, blank=True)
 
 
 def json_to_item_type(name: str, ob_taxonomy: dict):
@@ -55,3 +223,10 @@ def json_to_item_type(name: str, ob_taxonomy: dict):
         return ItemTypeUnit(name, it['description'], get_item_type_values('units', Unit))
     else:
         return ItemType(name, it['description'])
+
+
+def json_to_item_type_group(name: str, ob_taxonomy: dict):
+    if name == '':
+        return None
+    itg = ob_taxonomy['x-ob-item-type-groups'][name]
+    return ItemTypeGroup(itg['type'], itg['description'], tuple(itg['group']))
